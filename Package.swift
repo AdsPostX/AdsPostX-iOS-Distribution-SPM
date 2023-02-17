dedac4a987475941c8c251b2a115efbcd2b6ae88
shivang <shivang@enefits.co>
2023-02-17 19:28:27 +0530

    adding package details.
    adding package details.

--- Package.swift
+++ Package.swift
@@ -1,28 +1,22 @@
 // swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "AdsPostX",
+    platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AdsPostX",
             targets: ["AdsPostX"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .binaryTarget(
             name: "AdsPostX",
-            dependencies: []),
-        .testTarget(
-            name: "AdsPostXTests",
-            dependencies: ["AdsPostX"]),
+            url: "https://adpx.b-cdn.net/ios/1.0.0/AdsPostX.xcframework.zip",
+            checksum:
+                "5cf6d08231e3dca818b192ea7608036f8fefafd87c6755560d57090b7e491467")
     ]
 )
