4af3a21dd52e7f41fcb1d812019c421959219ee2
shivang <shivang@enefits.co>
2023-03-27 14:59:12 +0530

    updated adspostx.xcframework,  version: 1.4.0.
    updated adspostx.xcframework,  version: 1.4.0.

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdsPostX",
-            url: "https://adpx.b-cdn.net/ios/1.0.3/AdsPostX.xcframework.zip",
+            url: "https://adpx.b-cdn.net/ios/1.0.4/AdsPostX.xcframework.zip",
             checksum:
-                "088ab0bdc38173fb37ed99abdbbfb5c33ac3e8d22e1b204a77f29d44281387cc")
+                "e07c087058285e913f8b054aea7e3ffa770659e4db8056c6967c42af7eaa85fd")
     ]
 )
